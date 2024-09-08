8d51eb722bc8e296e06334eb66cc22c6f689d05f
imtiyaj ahamad <imbanty07@gmail.com>
2024-09-08 10:49:08 +0530

    final design

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
 import Header from "./components/header";
 import TodoList from "./components/TodoList";
-import bgMain from './assets/bgmain.svg';
-
+import bgMain from "./assets/bgmain.svg";
 
 const App = () => {
   return (
-    <div className=" w-100" style={
-      {
+    <div
+      className=" w-100"
+      style={{
         backgroundImage: `url(${bgMain})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        minHeight: '200px', 
-        color: 'black', 
-        height:'100vh'
-      }
-    }>
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+        color: "black",
+        height: "auto",
+      }}
+    >
+      <Header />
 
-   <Header/>
-  
-   <TodoList/>
+      <TodoList />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
